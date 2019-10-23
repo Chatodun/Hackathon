7b2d88be37aca77ba9c006c5f0b51c9b20854356
Chatodun <rangetopx@gmail.com>
2019-10-23 18:20:35 +0600

    Initial commit

---
+++ pharmacy/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=150, help_text='Название организации', verbose_name='Название организации')
+    delivery = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Branch(models.Model):
+    address = models.CharField(max_length=100, verbose_name='Адрес', help_text='Введите адрес')
+    phone = models.CharField(max_length=13, verbose_name='Телефон', help_text='Введите телефон')
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    organization = models.ForeignKey(Organization, verbose_name='Организация', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.organization.name},{self.address}'
+
+
+class Medicament(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название', help_text='Название препарата')
+    image = models.ImageField()
+    manual = models.TextField()
+    manufacturer = models.CharField(max_length=100)
+    purpose = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class MedicamentInPharmacy(models.Model):
+    pharmacy = models.ManyToManyField(Branch)
+    medicament = models.ForeignKey(Medicament, on_delete=models.CASCADE)
+    price = models.PositiveSmallIntegerField()
+    count = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.medicament.name
