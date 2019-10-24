ee12f5aa35c50776cb2833f8bc5778905300ce7c
rysbai <rysbai.coder@gmail.com>
2019-10-24 14:45:40 +0600

    Added endpoints for get branches by medicament_id

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -15,6 +15,16 @@ class BranchView(generics.ListCreateAPIView):
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
 
+    def get(self, request, *args, **kwargs):
+        medicament_id = request.query_params.get('medicament_id', None)
+        if medicament_id:
+            branches = self.queryset.filter(medicamentinbranch__medicament_id=medicament_id)
+        else:
+            branches = self.queryset.all()
+        serializer = self.serializer_class(branches, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
 
 class MedicamentView(generics.ListCreateAPIView):
     queryset = Medicament.objects.all()
@@ -23,10 +33,11 @@ class MedicamentView(generics.ListCreateAPIView):
     def get(self, request, *args, **kwargs):
         query = request.query_params.get('query', None)
         if query:
-            medicaments = self.queryset.filter(name__contains=query)
+            medicaments = self.queryset.filter(name__contains=query)[0]
+            serializer = self.serializer_class(medicaments)
         else:
             medicaments = self.queryset.all()
-        serializer = self.serializer_class(medicaments, many=True)
+            serializer = self.serializer_class(medicaments, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
